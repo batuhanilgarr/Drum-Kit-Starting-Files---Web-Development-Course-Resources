3882ae51bd8d63414f339bc566cec52a9c78b24c
batuhanilgarr <batuhan.ilgar@8bitiz.com>
2022-07-28 15:08:25 +0300

    #difficult

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Detecting Button Press
 var numberOfDrumButtons = document.querySelectorAll('.drum').length
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
@@ -7,49 +8,82 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
     var buttonInnerHTML = this.innerHTML
 
-    // Bulunduğumuz ifadeyi yada olduğumuz şeyi içerir
-    switch (buttonInnerHTML) {
-      //değiştirilecek element
-      case 'w':
-        var tom1 = new Audio('sounds/tom-1.mp3')
-        tom1.play()
-        break
+    makeSound(buttonInnerHTML);
 
-      case 'a':
-        var tom2 = new Audio('sounds/tom-2.mp3')
-        tom2.play()
-        break
+    buttonAnimation( buttonInnerHTML);
 
-      case 's':
-        var tom3 = new Audio('sounds/tom-3.mp3')
-        tom3.play()
-        break
+  });
 
-      case 'd':
-        var tom4 = new Audio('sounds/tom-4.mp3')
-        tom4.play()
-        break
-
-      case 'j':
-        var snare = new Audio('sounds/snare.mp3')
-        snare.play()
-        break
-
-      case 'k':
-        var crash = new Audio('sounds/crash.mp3')
-        crash.play()
-        break
-
-      case 'l':
-        var kick = new Audio('sounds/kick-bass.mp3')
-        kick.play()
-        break
-
-      default:
-        console.log(buttonInnerHTML)
-    }
-  })
 }
+
+//Detecting Keyboard Press
+
+document.addEventListener("keypress", function(event){
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+});
+
+
+function makeSound(key) {
+
+  // Bulunduğumuz ifadeyi yada olduğumuz şeyi içerir
+  switch (key) {
+    //değiştirilecek element
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3')
+      tom1.play()
+      break
+
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3')
+      tom2.play()
+      break
+
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3')
+      tom3.play()
+      break
+
+    case 'd':
+      var tom4 = new Audio('sounds/tom-4.mp3')
+      tom4.play()
+      break
+
+    case 'j':
+      var snare = new Audio('sounds/snare.mp3')
+      snare.play()
+      break
+
+    case 'k':
+      var crash = new Audio('sounds/crash.mp3')
+      crash.play()
+      break
+
+    case 'l':
+      var kick = new Audio('sounds/kick-bass.mp3')
+      kick.play()
+      break
+
+    default:
+      console.log(buttonInnerHTML)
+  }
+}
+
+
+function buttonAnimation(currentKey){
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100)
+
+}
+
+
 
 // var audio = new Audio('sounds/tom-1.mp3')
 // audio.play()
@@ -90,3 +124,49 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 //  pickUpSuitecase();
 //  moveBy();
 // }
+
+
+
+
+
+
+
+
+// function HouseKeeper(yearsOfExperience, name, cleaningRepertoire) {
+//   this.yearsOfExperience = yearsOfExperience
+//   this.name = name
+//   this.cleaningRepertoire = cleaningRepertoire
+//   this.clean = function () {
+//     alert('Cleaning in progress')
+//   }
+// }
+
+
+// var houseKeeper1 = new HouseKeeper(14, 'Janet', ['bedroom'])
+
+
+// houseKeeper1.clean()
+
+
+
+// Constructor Function
+// function Audio (fileLocation){
+//   this.fileLocation = fileLocation;
+//   this.play = function () {
+  
+//   }
+// }
+
+
+
+
+
+
+// function sayHi(to) {
+//   console.log('Hello, ' + to)
+// }
+
+// sayHi('Tom')
+
+
+// Hello, Tom
